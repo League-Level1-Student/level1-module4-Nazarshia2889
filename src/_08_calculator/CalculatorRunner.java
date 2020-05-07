2180886fe57121da8171f55e8e8073abebf13181
Arshia Nayebnazar <arshianayebnazar@Arshias-MacBook-Air.local>
2020-05-06 20:32:29 -0700

    extra

--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -2,8 +2,7 @@ package _08_calculator;
 
 public class CalculatorRunner {
 	public static void main(String[] args) {
-		calculator c = new calculator(5, 10);
-		System.out.println(c.add());
+		
 	}
 
 }
--- src/_08_calculator/calculator.java
+++ src/_08_calculator/calculator.java
@@ -1,8 +1,37 @@
 package _08_calculator;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class calculator {
 	int x;
 	int y;
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addition = new JButton();
+	JButton subtraction = new JButton();
+	JButton multiplication = new JButton();
+	JButton division = new JButton();
+	
+	
+	void run() {
+		frame.setVisible(true);
+		frame.setTitle("Calculator");
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(addition);
+		panel.add(subtraction);
+		panel.add(multiplication);
+		panel.add(division);
+		
+		addition.setText("Add");
+		subtraction.setText("Subtract");
+		multiplication.setText("Multiply");
+		division.setText("Divide");
+		
+		
+	}
 	
 	calculator(int firstNumber, int secondNumber){
 		x = firstNumber;
