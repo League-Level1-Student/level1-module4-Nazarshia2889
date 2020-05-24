a8822254af3a5e2d55d29f6b8a2c72a12e572ab2
Arshia Nayebnazar <arshianayebnazar@Arshias-MacBook-Air.local>
2020-05-24 14:17:08 -0700

    extra

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,5 +1,20 @@
 package _11_whack_a_mole;
 
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class WhackAMole {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	Random ran = new Random();
+	int x = ran.nextInt(24);
+	
+	
+	
+	
 
 }
--- src/_11_whack_a_mole/WhackAMoleRunner.java
+++ src/_11_whack_a_mole/WhackAMoleRunner.java
@@ -1,5 +1,8 @@
 package _11_whack_a_mole;
 
 public class WhackAMoleRunner {
+	public static void main(String[] args) {
+		
+	}
 
 }
