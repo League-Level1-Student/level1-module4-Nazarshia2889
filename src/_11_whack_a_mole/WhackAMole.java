ed053ad01d0441e52c3e139f5044271566e231ca
Arshia Nayebnazar <arshianayebnazar@Arshias-MacBook-Air.local>
2020-05-24 17:41:50 -0700

    new

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,20 +1,91 @@
 package _11_whack_a_mole;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
+	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button = new JButton();
+	JButton button;
+	JButton b;
 	Random ran = new Random();
-	int x = ran.nextInt(24);
-	
-	
-	
-	
+	int y = 0;
 
+	void run() {
+		setFrame(panel);
+		frame.add(panel);
+		drawButtons();
+	}
+	
+	void setFrame(JPanel panel) {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setTitle("Whack a Mole");
+		frame.setSize(300, 300);
+	}
+	
+	public void drawButtons() {
+		frame.remove(panel);
+		panel = new JPanel();
+		frame.add(panel);
+		int x = ran.nextInt(24);
+		for(int i = 0; i < 24; i++) {
+			if(i == x) {
+				b = new JButton();
+				b.setText("mole!");
+				panel.add(b);
+				b.addActionListener(this);
+			}
+			else {
+				button = new JButton();
+				panel.add(button);
+				button.addActionListener(this);
+			}
+		}
+		setFrame(panel);
+		frame.add(panel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		drawButtons();
+		
+		if(buttonPressed.getText().equals("")) {
+			speak("No");
+			System.out.println("no");
+		}
+		else if(buttonPressed.getText().equals("mole!")) {
+			speak("yes");
+		}
+		
+		
+		
+	}
+	void speak(String words) {
+        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+                    + words + "');\"";
+            try {
+                Runtime.getRuntime().exec( cmd ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Runtime.getRuntime().exec( "say " + words ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+	
+	
+        }
+	}
 }
--- src/_11_whack_a_mole/WhackAMoleRunner.java
+++ src/_11_whack_a_mole/WhackAMoleRunner.java
@@ -2,7 +2,7 @@ package _11_whack_a_mole;
 
 public class WhackAMoleRunner {
 	public static void main(String[] args) {
-		
+		new WhackAMole().run();
 	}
 
 }
